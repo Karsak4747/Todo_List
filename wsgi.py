050f320914b79223ba043cea35ffd5a6ae356e03
Karsak <saakov@infochemistry.ru>
2023-11-30 00:32:49 +0300

    Оно работает!

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Todo_List-.todowoo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todowoo.settings')
 
 application = get_wsgi_application()
